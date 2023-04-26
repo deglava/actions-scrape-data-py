203f3a0ead7a8091b5f807bebc32fbe43ba1f132
deglava <deglava@yahoo.com>
2023-04-25 22:32:07 -0400

    update workflow

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def write_to_excel(rows):
         csvwriter.writerows(rows)
 
 if __name__ == "__main__":
-    date_format = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    date_format = datetime.now(EST).strftime("%d/%m/%Y %H:%M:%S")
     print("MSFT current price: " + "$" + str(date_format) + " is $" + str(t.tickers['MSFT'].info['currentPrice']))
     
     rows = [[str(date_format), 'AAPL', "$" + str(t.tickers['AAPL'].info['currentPrice'])],
